5bbb2839d293c2b6b553d8f81f2eb6cbbfaa8ce5
GrupoRivero <irodriguez@gruporivero.com>
2014-01-10 16:41:50 -0600

    Agregar div animado

--- js/script.js
+++ js/script.js
@@ -1,109 +1,50 @@
-/*
-*	jquery.dropdown.js
-*	Apply sexy dropdowns on any ul with child ul.
-*	Depends on: jquery.hoverIntent.js
-*/
 
-$.fn.dropdown = function(options) {
+var ww = document.body.clientWidth;
 
-	var defaults = {};
-	var opts = $.extend(defaults, options);
+$(document).ready(function() {
+	$(".nav li a").each(function() {
+		if ($(this).next().length > 0) {
+			$(this).addClass("parent");
+		};
+	})
 	
-	// Apply class=hasChildren on those items with children
-	this.each(function() {
-		$(this).find('li').each(function() {
-			if($(this).find("ul").length > 0) {
-				$(this).addClass("hasChildren");
-				$(this).find('> a').wrapInner("<span></span>");
-			}
-		});
+	$(".toggleMenu").click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass("active");
+		$(".nav").toggle();
 	});
-	
-	// Apply class=hover on all list items
-	$(this).find("li").on('hover', function() {
-		$(this).addClass('hover');
-	}, function() {
-		$(this).removeClass('hover');
-	});
-};
+	adjustMenu();
+})
 
-/*
-*	When the DOM is ready,
-*	Let's get cracking.
-*/
+$(window).bind('resize orientationchange', function() {
+	ww = document.body.clientWidth;
+	adjustMenu();
+});
 
-$(function() {
-
-	// Calling the jquery dropdown
-	$('nav').dropdown();
-		
-	
-	/*
-	*	Applicable only to Desktop version of the Navigation
-	*/
-		
-		// For IE versions 7 or less, make the navigation appear on hover.
-		if ($.browser.msie && $.browser.version.substr(0,1)<7) {
-			$('li').has('ul').mouseover(function(){
-				$(this).children('ul').show();
-			}).mouseout(function(){
-				$(this).children('ul').hide();
-			});
+var adjustMenu = function() {
+	if (ww < 768) {
+		$(".toggleMenu").css("display", "inline-block");
+		if (!$(".toggleMenu").hasClass("active")) {
+			$(".nav").hide();
+		} else {
+			$(".nav").show();
 		}
-		
-	
-	/*
-	*	Applicable only to Mobile-nav
-	*/
-	
-		// Checking if the top-right button is visible.
-		if ($("nav a.btn-navbar").is(":visible")) {
-			
-			// Making the dropdown magically appear onclick/touch.
-			$('nav a.btn-navbar').on('click', function() {
-				$('ul#menu').slideToggle('fast', function() {
-					$('ul#menu').css({
-						
-						// The height must be fixed for the native scrolling on iOS
-						'height': $(this).height(),
-						
-						// But we don't want the height of the nav to exceed the viewport.
-						'max-height': $(window).height() + 20
-					});
-				});
-				
-				$(this).toggleClass('active');
-						
-			}); // end on.click
-			
-			// Making the children appear on click/touch
-			$('ul#menu li.hasChildren a').on('click', function() {
-				$(this).parent().children('ul').slideToggle('fast', function() {
-				
-				$('ul#menu').css({
-					
-					// Resetting the height to auto in order to expand/contract the menu upon interaction.
-					'height': 'auto',
-					
-					// But we don't want the height of the nav to exceed the viewport.
-					'max-height': $(window).height() + 20
-				});
-								
-				}); // end slideToggle
-				
-			}); // end on.click
-			
-		} // end visibility check
-	
-});
+		$(".nav li").unbind('mouseenter mouseleave');
+		$(".nav li a.parent").unbind('click').bind('click', function(e) {
+			// must be attached to anchor element to prevent bubbling
+			e.preventDefault();
+			$(this).parent("li").toggleClass("hover");
+		});
+	} 
+	else if (ww >= 768) {
+		$(".toggleMenu").css("display", "none");
+		$(".nav").show();
+		$(".nav li").removeClass("hover");
+		$(".nav li a").unbind('click');
+		$(".nav li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+		 	// must be attached to li so that mouseleave is not triggered when hover over submenu
+		 	$(this).toggleClass('hover');
+		});
+	}
+}
 
-
-/*
-*	Display the Screen Width
-*/
-
-$("#dimensions .width").html($(window).width());
-
-$(window).resize(function() {
-  $("#dimensions .width").html($(window).width());
-});
